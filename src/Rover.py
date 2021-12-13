ee3874ec697442f35416bbcb9f140a70b5af150f
Abraham Vallez <abevallez@gmail.com>
2021-12-13 16:11:05 +0100

    refactor Rover

--- src/Rover.py
+++ src/Rover.py
@@ -2,6 +2,7 @@ from src.Position import Position
 
 
 class Rover:
+
     def __init__(self, position: Position):
         self._position: Position = position
 
@@ -11,27 +12,41 @@ class Rover:
 
     def move_forward(self):
         if self._position.direction == 'N':
-            new_position = Position(self._position.coordinates[0] + 1,
-                                    self._position.coordinates[1],
-                                    self._position.direction)
+            new_position = self._move_forward_x()
         elif self._position.direction == 'E':
-            new_position = Position(self._position.coordinates[0],
-                                    self._position.coordinates[1] + 1,
-                                    self._position.direction)
+            new_position = self._move_forward_y()
         elif self._position.direction == 'S':
-            new_position = Position(self._position.coordinates[0] - 1,
-                                    self._position.coordinates[1],
-                                    self._position.direction)
+            new_position = self.move_back_x()
         elif self._position.direction == 'W':
-            new_position = Position(self._position.coordinates[0],
-                                    self._position.coordinates[1] - 1,
-                                    self._position.direction)
+            new_position = self._move_back_y()
         self._position = new_position
 
-    def move_back(self):
+    def _move_back_y(self):
+        new_position = Position(self._position.coordinates[0],
+                                self._position.coordinates[1] - 1,
+                                self._position.direction)
+        return new_position
+
+    def move_back_x(self):
         new_position = Position(self._position.coordinates[0] - 1,
                                 self._position.coordinates[1],
                                 self._position.direction)
+        return new_position
+
+    def _move_forward_y(self):
+        new_position = Position(self._position.coordinates[0],
+                                self._position.coordinates[1] + 1,
+                                self._position.direction)
+        return new_position
+
+    def _move_forward_x(self):
+        new_position = Position(self._position.coordinates[0] + 1,
+                                self._position.coordinates[1],
+                                self._position.direction)
+        return new_position
+
+    def move_back(self):
+        new_position = self.move_back_x()
         self._position = new_position
 
     def turn_right(self):
@@ -67,3 +82,4 @@ class Rover:
 
     def current_coordinates(self) -> list:
         return self._position.coordinates
+
