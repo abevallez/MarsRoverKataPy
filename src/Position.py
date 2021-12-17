8447bfc1cabef6f259a61d40766a5c9edf98736a
Abraham Vallez <abevallez@gmail.com>
2021-12-17 21:42:05 +0100

    refactor turn to position

--- src/Position.py
+++ src/Position.py
@@ -20,23 +20,23 @@ class Position:
     def world(self) -> World:
         return self._world
 
-    def is_start_x_edge(self) -> bool:
+    def _is_start_x_edge(self) -> bool:
         return self._coordinates[0] == 0
 
-    def is_end_x_edge(self) -> bool:
+    def _is_end_x_edge(self) -> bool:
         return self._coordinates[0] == self._world.size_y - 1
 
-    def is_start_y_edge(self) -> bool:
+    def _is_start_y_edge(self) -> bool:
         return self._coordinates[1] == 0
 
-    def is_end_y_edge(self) -> bool:
+    def _is_end_y_edge(self) -> bool:
         return self._coordinates[1] == self._world.size_y - 1
 
     def is_facing(self, direction: str) -> bool:
         return self._direction == direction
 
-    def move_forward_x(self):
-        if self.is_end_x_edge():
+    def _move_forward_x(self):
+        if self._is_end_x_edge():
             new_x = 0
         else:
             new_x = self.coordinates[0] + 1
@@ -47,8 +47,8 @@ class Position:
                                 self.world)
         return new_position
 
-    def move_forward_y(self):
-        if self.is_end_y_edge():
+    def _move_forward_y(self):
+        if self._is_end_y_edge():
             new_y = 0
         else:
             new_y = self.coordinates[1] + 1
@@ -59,8 +59,8 @@ class Position:
                                 self.world)
         return new_position
 
-    def move_back_x(self):
-        if self.is_start_x_edge():
+    def _move_back_x(self):
+        if self._is_start_x_edge():
             new_x = 5
         else:
             new_x = self.coordinates[0] - 1
@@ -71,8 +71,8 @@ class Position:
                                 self.world)
         return new_position
 
-    def move_back_y(self):
-        if self.is_start_y_edge():
+    def _move_back_y(self):
+        if self._is_start_y_edge():
             new_y = 5
         else:
             new_y = self.coordinates[1] - 1
@@ -83,4 +83,53 @@ class Position:
                                 self.world)
         return new_position
 
+    def move_forward(self):
+        if self._direction == 'N':
+            position = self._move_forward_x()
+        elif self._direction == 'E':
+            position = self._move_forward_y()
+        elif self._direction == 'S':
+            position = self._move_back_x()
+        elif self._direction == 'W':
+            position = self._move_back_y()
+        return position
+
+    def move_back(self):
+        if self._direction == 'N':
+            position = self._move_back_x()
+        elif self._direction == 'E':
+            position = self._move_back_y()
+        elif self._direction == 'S':
+            position = self._move_forward_x()
+        elif self._direction == 'W':
+            position = self._move_forward_y()
+        return position
+
+    def turn_right(self):
+        if self._direction == 'N':
+            new_direction = 'E'
+        elif self._direction == 'E':
+            new_direction = 'S'
+        elif self._direction == 'S':
+            new_direction = 'W'
+        elif self._direction == 'W':
+            new_direction = 'N'
+        return Position(self._coordinates[0],
+                                self._coordinates[1],
+                                new_direction,
+                                self._world)
+
+    def turn_left(self):
+        if self._direction == 'N':
+            new_direction = 'W'
+        elif self._direction == 'W':
+            new_direction = 'S'
+        elif self._direction == 'S':
+            new_direction = 'E'
+        elif self._direction == 'E':
+            new_direction = 'N'
+        return Position(self._coordinates[0],
+                                self._coordinates[1],
+                                new_direction,
+                                self._world)
 
--- src/Rover.py
+++ src/Rover.py
@@ -16,54 +16,16 @@ class Rover:
         return self._position
 
     def move_forward(self):
-        if self._position.direction == 'N':
-            self._position = self._position.move_forward_x()
-        elif self._position.direction == 'E':
-            self._position = self._position.move_forward_y()
-        elif self._position.direction == 'S':
-            self._position = self._position.move_back_x()
-        elif self._position.direction == 'W':
-            self._position = self._position.move_back_y()
+        self._position = self._position.move_forward()
 
     def move_back(self):
-        if self._position.direction == 'N':
-            self._position = self._position.move_back_x()
-        elif self._position.direction == 'E':
-            self._position = self._position.move_back_y()
-        elif self._position.direction == 'S':
-            self._position = self._position.move_forward_x()
-        elif self._position.direction == 'W':
-            self._position = self._position.move_forward_y()
+        self._position = self._position.move_back()
 
     def turn_right(self):
-        if self._position.direction == 'N':
-            new_direction = 'E'
-        elif self._position.direction == 'E':
-            new_direction = 'S'
-        elif self._position.direction == 'S':
-            new_direction = 'W'
-        elif self._position.direction == 'W':
-            new_direction = 'N'
-        new_position = Position(self._position.coordinates[0],
-                                self._position.coordinates[1],
-                                new_direction,
-                                self._position.world)
-        self._position = new_position
+        self._position = self._position.turn_right()
 
     def turn_left(self):
-        if self._position.direction == 'N':
-            new_direction = 'W'
-        elif self._position.direction == 'W':
-            new_direction = 'S'
-        elif self._position.direction == 'S':
-            new_direction = 'E'
-        elif self._position.direction == 'E':
-            new_direction = 'N'
-        new_position = Position(self._position.coordinates[0],
-                                self._position.coordinates[1],
-                                new_direction,
-                                self._position.world)
-        self._position = new_position
+        self._position = self._position.turn_left()
 
     def current_direction(self) -> str:
         return self._position.direction
